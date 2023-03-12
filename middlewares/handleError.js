b2d535c8c9fc0ea76baa01d2b25045ed24439602
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2023-03-12 15:35:28 +0300

    fix after first review 2, 8

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,37 +1,32 @@
 const mongoose = require('mongoose');
 const ValidationError = require('../errors/ValidationError');
-const InternalServerError = require('../errors/InternalServerError');
-const NotFoundError = require('../errors/NotFoundError');
-const UnauthorizedError = require('../errors/UnauthorizedError');
 const ConflictedError = require('../errors/ConflictedError');
-const ForbiddenError = require('../errors/ForbiddenError');
+const NotFoundError = require('../errors/NotFoundError');
 
-function handleError(error, req, res, next) {
+function handleError(error, _, res, next) {
   let err = error;
-  switch (true) {
-    case error.message === 'Validation failed':
-      err = new ValidationError('Incorrect data');
-      break;
-    case error.code === 11000:
-      err = new ConflictedError('Such a user already exists');
-      break;
-    case error instanceof mongoose.Error.ValidationError:
-      err = new ValidationError('Incorrect data');
-      break;
-    case error instanceof mongoose.Error.CastError:
-      err = new ValidationError('Incorrect data');
-      break;
-    case error instanceof NotFoundError:
-      break;
-    case error instanceof ForbiddenError:
-      break;
-    case error instanceof UnauthorizedError:
-      break;
-    case error instanceof ValidationError:
-      break;
-    default:
-      err = new InternalServerError();
-      break;
+
+  if (error instanceof mongoose.Error) {
+    switch (true) {
+      case error instanceof mongoose.Error.DocumentNotFoundError:
+        err = new NotFoundError('Data not found');
+        break;
+      default:
+        err = new ValidationError('Incorrect data');
+        break;
+    }
+  } else {
+    switch (true) {
+      case error.code === 11000:
+        err = new ConflictedError('Such a user already exists');
+        break;
+      default:
+        if (!error.status) {
+          err.status = 500;
+          err.message = 'Internal server error';
+        }
+        break;
+    }
   }
   res.status(err.status);
   res.send({ message: err.message });
